3226ad0c9f6029458152fe790b46b6a65a567a02
mahebish <mahe.bisht87@gmail.com>
2024-06-23 20:21:38 +0530

    updated files

---
+++ app.py
@@ -0,0 +1,44 @@
+from fastapi import FastAPI, Form, Request, Response, File, Depends, HTTPException, status
+from fastapi.responses import RedirectResponse
+from fastapi.staticfiles import StaticFiles
+from fastapi.templating import Jinja2Templates
+from fastapi.encoders import jsonable_encoder
+import uvicorn  
+import os
+import aiofiles
+import json
+import csv
+
+
+
+app = FastAPI()
+
+app.mount("/static", StaticFiles(directory="static"), name="static")
+
+templates = Jinja2Templates(directory="templates")
+
+
+@app.get("/")
+async def home(request: Request):
+    return templates.TemplateResponse("index.html", {"request": request})
+
+
+@app.post("/upload")
+async def upload_file(request: Request, pdf_file: bytes = File(),filename: str = Form(...)):
+    base_folder = "static/docs/"
+    if not os.path.isdir(base_folder):
+        os.mkdir(base_folder)
+    pdf_filename = os.path.join(base_folder, filename)
+
+    async with aiofiles.open(pdf_filename, mode="wb") as f:
+        await f.write(pdf_file)
+
+    response_data = jsonable_encoder(json.dumps({"msg": 'success', "pdf_filename": pdf_filename}))
+    res = Response(response_data)
+    return res
+
+    
+
+
+if __name__ == "__main__":
+    uvicorn.run("app:app", host="0.0.0.0", port=8000, reload = True)
